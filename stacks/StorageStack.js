e9158b1c366aff3db86a10f25272f74dda7fdbbe
tamlee <tamlee448@gmail.com>
2023-04-26 15:32:44 -0700

    create an S3 bucket in SST

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,6 +1,8 @@
-import { Table } from "sst/constructs";
+import { Bucket, Table } from "sst/constructs";
 
 export function StorageStack({ stack, app }) {
+    // Create an S3 bucket
+const bucket = new Bucket(stack, "Uploads");
   // Create the DynamoDB table
   const table = new Table(stack, "Notes", {
     fields: {
@@ -12,5 +14,6 @@ export function StorageStack({ stack, app }) {
 
   return {
     table,
+    bucket
   };
 }
