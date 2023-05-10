999c8ca912e4edfbe296302e9e020ea2278d1181
tamlee <tamlee448@gmail.com>
2023-05-10 16:31:01 -0700

    saving for now

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './containers/Home';
+import Login from './containers/Login';
 import NotFound from './containers/NotFound';
 
 export default function Links() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
             <Route path="*" element={<NotFound />} />;
         </Routes>
     );
