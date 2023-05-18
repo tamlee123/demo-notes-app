2c26033e50ce35aa7fc588d9bf5436ad3e4c53bc
tamlee <tamlee448@gmail.com>
2023-05-18 13:50:49 -0700

    create new note form-content,s3 attachment

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './containers/Home';
 import Login from './containers/Login';
+import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
 import NotFound from './containers/NotFound';
 
 export default function Links() {
@@ -9,6 +11,8 @@ export default function Links() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/notes/new" element={<NewNote />} />
             <Route path="*" element={<NotFound />} />;
         </Routes>
     );
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { useAppContext } from '../lib/contextLib';
+import { onError } from '../lib/errorLib';
 import './Home.css';
 
 export default function Home() {
-    return (
-        <div className="Home">
+    const [notes, setNotes] = useState([]);
+    const { isAuthenticated } = useAppContext();
+    const [isLoading, setIsLoading] = useState(true);
+
+    function renderNotesList(notes) {
+        return null;
+    }
+
+    function renderLander() {
+        return (
             <div className="lander">
                 <h1>Scratch</h1>
                 <p className="text-muted">A simple note taking app</p>
             </div>
-        </div>
-    );
+        );
+    }
+
+    function renderNotes() {
+        return (
+            <div className="notes">
+                <h2 className="pb-3 mt-4 mb-3 border-bottom">Your Notes</h2>
+                <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
+            </div>
+        );
+    }
+
+    return <div className="Home">{isAuthenticated ? renderNotes() : renderLander()}</div>;
 }
