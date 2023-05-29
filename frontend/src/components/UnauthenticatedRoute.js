5278546bcdbae7c977c399e01d2bce1aeef5b2af
tamlee <tamlee448@gmail.com>
2023-05-29 16:27:19 -0700

    redirect users to the page they want after they are logged in

--- frontend/src/components/UnauthenticatedRoute.js
+++ frontend/src/components/UnauthenticatedRoute.js
@@ -2,12 +2,27 @@ import React, { cloneElement } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAppContext } from '../lib/contextLib';
 
+//Extract param value from the url
+function querystring(name, url = window.location.href) {
+    const parsedName = name.replace(/[[]]/g, '\\$&');
+    const regex = new RegExp(`[?&]${parsedName}(=([^&#]*)|&|#|$)`, 'i');
+    const results = regex.exec(url);
+
+    if (!results || !results[2]) {
+        return false;
+    }
+
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 export default function UnauthenticatedRoute(props) {
     const { isAuthenticated } = useAppContext();
     const { children } = props;
+    const redirect = querystring('redirect');
 
+    //redirect the users to the page they want after they are logged in
     if (isAuthenticated) {
-        return <Navigate to={'/'} />;
+        return <Navigate to={redirect || '/'} />;
     }
 
     return cloneElement(children, props);
